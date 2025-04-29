16ca53ffaf7db9445a2fb7a3148ba44818762815
Fernando Lucena <fernando.lucena@utalca.cl>
2025-04-28 20:50:01 -0400

    chore: creacion de config

---
+++ src/core/configs/joi.config.ts
@@ -0,0 +1,18 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  APP_PORT: Joi.number().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_LOGGING: Joi.string().required(),
+  DB_SYNCHRONIZE: Joi.string().required(),
+  JWT_SECRET_KEY: Joi.string().required(),
+  JWT_EXPIRATION_TIME: Joi.string().required(),
+  CORS_ORIGINS: Joi.string().required(),
+  SSL_CERT_FILE: Joi.string().required(),
+  SSL_KEY_FILE: Joi.string().required(),
+  SSL_CA_FILE: Joi.string().required(),
+});
