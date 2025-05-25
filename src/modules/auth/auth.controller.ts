9400aeff46d1c6d250dc7d4c198b918e573b142e
Fernando Lucena <fernandolucena83@gmail.com>
2025-05-25 17:30:02 -0400

    feat: inicializacion de jwt

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,10 +8,15 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  login(@Body() loginAuthDto: LoginAuthDto) {
+    return this.authService.login(loginAuthDto);
+  }
 }
