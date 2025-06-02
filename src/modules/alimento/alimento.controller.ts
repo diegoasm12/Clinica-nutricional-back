c04b98b7b456a344cec979a7bebd551ba5dba1c3
Fernando Lucena <fernandolucena83@gmail.com>
2025-06-01 20:32:22 -0400

    fix: eliminacion dato

--- src/modules/alimento/alimento.controller.ts
+++ src/modules/alimento/alimento.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AlimentoService } from './alimento.service';
 import { CreateAlimentoDto } from './dto/create-alimento.dto';
 import { UpdateAlimentoDto } from './dto/update-alimento.dto';
@@ -6,29 +14,4 @@ import { UpdateAlimentoDto } from './dto/update-alimento.dto';
 @Controller('alimento')
 export class AlimentoController {
   constructor(private readonly alimentoService: AlimentoService) {}
-
-  @Post()
-  create(@Body() createAlimentoDto: CreateAlimentoDto) {
-    return this.alimentoService.create(createAlimentoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.alimentoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.alimentoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlimentoDto: UpdateAlimentoDto) {
-    return this.alimentoService.update(+id, updateAlimentoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.alimentoService.remove(+id);
-  }
 }
--- src/modules/tipo_comida/tipo_comida.controller.ts
+++ src/modules/tipo_comida/tipo_comida.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TipoComidaService } from './tipo_comida.service';
 import { CreateTipoComidaDto } from './dto/create-tipo_comida.dto';
 import { UpdateTipoComidaDto } from './dto/update-tipo_comida.dto';
@@ -6,29 +14,4 @@ import { UpdateTipoComidaDto } from './dto/update-tipo_comida.dto';
 @Controller('tipo-comida')
 export class TipoComidaController {
   constructor(private readonly tipoComidaService: TipoComidaService) {}
-
-  @Post()
-  create(@Body() createTipoComidaDto: CreateTipoComidaDto) {
-    return this.tipoComidaService.create(createTipoComidaDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.tipoComidaService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tipoComidaService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTipoComidaDto: UpdateTipoComidaDto) {
-    return this.tipoComidaService.update(+id, updateTipoComidaDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tipoComidaService.remove(+id);
-  }
 }
