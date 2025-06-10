93b9e8303eb91ff9fc5166c59313517c4e5a0bb5
Fernando Lucena <fernando.lucena@utalca.cl>
2025-06-09 20:30:31 -0400

    refactor: limpieza

--- src/modules/rol/rol.service.ts
+++ src/modules/rol/rol.service.ts
@@ -3,24 +3,4 @@ import { CreateRolDto } from './dto/create-rol.dto';
 import { UpdateRolDto } from './dto/update-rol.dto';
 
 @Injectable()
-export class RolService {
-  create(createRolDto: CreateRolDto) {
-    return 'This action adds a new rol';
-  }
-
-  findAll() {
-    return `This action returns all rol`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} rol`;
-  }
-
-  update(id: number, updateRolDto: UpdateRolDto) {
-    return `This action updates a #${id} rol`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} rol`;
-  }
-}
+export class RolService {}
